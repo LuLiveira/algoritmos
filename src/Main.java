ea9f62206e101725634f9c78be77bd043bc0d706
Lucas Silva <lcsd.lucas@gmail.com>
2025-01-10 22:59:28 -0300

    refatorando busca binaria

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+List<Integer> numeros = IntStream.rangeClosed(1, 100)
+        .boxed()
+        .toList();
+List<String> nomes = List.of(
+        "Ana",        // A
+        "Bruno",      // B
+        "Carlos",     // C
+        "Daniela",    // D
+        "Eduardo",    // E
+        "Fernanda",   // F
+        "Gabriel",    // G
+        "Helena",     // H
+        "Isabela",    // I
+        "João",       // J
+        "Karla",      // K
+        "Lucas",      // L
+        "Mariana",    // M
+        "Natalia",    // N
+        "Otávio",     // O
+        "Paula",      // P
+        "Quirino",    // Q
+        "Rafael",     // R
+        "Sofia",      // S
+        "Tiago",      // T
+        "Úrsula",     // U
+        "Victor",     // V
+        "William",    // W
+        "Xavier",     // X
+        "Yasmin",     // Y
+        "Zara"        // Z
+);
 
 public void main() {
     System.out.println("Hello, World!");
@@ -8,13 +39,8 @@ public void main() {
     binary_search();
 }
 
-
 // O(log n)
 public void binary_search() {
-    List<Integer> numeros = IntStream.rangeClosed(1, 100)
-            .boxed()
-            .toList();
-
     var target = 7;
     var posicao = 0;
     var posicaoBaixa = 0;
@@ -42,36 +68,8 @@ public void binary_search() {
     }
     System.out.println(STR."A posicao do numero \{target} é: \{posicao}");
 
-    List<String> nomes = List.of(
-            "Ana",        // A
-            "Bruno",      // B
-            "Carlos",     // C
-            "Daniela",    // D
-            "Eduardo",    // E
-            "Fernanda",   // F
-            "Gabriel",    // G
-            "Helena",     // H
-            "Isabela",    // I
-            "João",       // J
-            "Karla",      // K
-            "Lucas",      // L
-            "Mariana",    // M
-            "Natalia",    // N
-            "Otávio",     // O
-            "Paula",      // P
-            "Quirino",    // Q
-            "Rafael",     // R
-            "Sofia",      // S
-            "Tiago",      // T
-            "Úrsula",     // U
-            "Victor",     // V
-            "William",    // W
-            "Xavier",     // X
-            "Yasmin",     // Y
-            "Zara"        // Z
-    );
 
-    var nomeTarget = "Lucas";
+    var nomeTarget = "Zara";
     var posicaoDoNome = 0;
 
     var inicio = 0;
